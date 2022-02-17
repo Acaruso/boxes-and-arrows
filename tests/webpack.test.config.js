f7e782e562171df2be7b90e9e2cb18d3d397bd3b
Acaruso <anthonycaruso102@gmail.com>
2022-02-17 16:59:31 -0500

    stuff

--- tests/webpack.test.config.js
+++ tests/webpack.test.config.js
@@ -2,12 +2,11 @@ const path = require('path');
 const glob = require('glob');
 
 const testFiles = glob
-    .sync("./test/*.spec.js")
-    .filter((element) => element !== "test/bundle.test.js");
+    .sync("./tests/*.spec.js")
+    .filter((element) => element !== "tests/bundle.test.js");
 
 module.exports = {
     entry: testFiles,
-    // entry: "./test/myTest.spec.js",
     output: {
         path: path.resolve(__dirname, "."),
         filename: "bundle.test.js"
