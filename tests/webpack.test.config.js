bcc3c0c051df2b21fb24798b5681eff11f89b507
Acaruso <anthonycaruso102@gmail.com>
2022-02-17 17:03:06 -0500

    stuff

--- tests/webpack.test.config.js
+++ tests/webpack.test.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 const glob = require('glob');
 
+// Our entry point's path is relative to where we are running Webpack,
+// but our output path is relative to where the Webpack configuration lives
+
 const testFiles = glob
     .sync("./tests/*.spec.js")
-    .filter((element) => element !== "tests/bundle.test.js");
+    .filter(x => x !== "tests/bundle.test.js");
 
 module.exports = {
     entry: testFiles,
