23e1e3fa296077e5b7583407aaa975471920ff8e
Acaruso <anthonycaruso102@gmail.com>
2022-02-26 15:27:55 -0500

    stuff

--- src/tree_formatter.js
+++ src/tree_formatter.js
@@ -74,8 +74,96 @@ class TreeFormatter {
             }
         }
 
-        // todo: check for conflicts
+        if (childIds.length > 0 && this.boxes.isLeftMost(id)) {
+            this.checkForConflicts(id);
+        }
     }
+
+    checkForConflicts(id) {
+        const box = this.boxes.getBox(id);
+        let siblingId = this.boxes.getLeftMostSibling(id);
+        let sibling = this.boxes.getBox(siblingId);
+        const childIds = this.boxes.getConnections(id);
+
+        const minDistance = this.xPadding;
+        let shiftValue = 0;
+        let nodeContour = new Map();
+        this.getLeftContour(id, 0, nodeContour);
+
+        while (siblingId !== null && siblingId !== id) {
+            let siblingContour = new Map();
+            this.getRightContour(siblingId, 0, siblingContour);
+            
+            const sMax = getMaxKey(siblingContour);
+            const nMax = getMaxKey(nodeContour);
+
+            for (
+                let level = box.coord.y + 1;
+                level <= Math.min(sMax, nMax);
+                level++
+            ) {
+                let distance = nodeContour.get(level) - siblingContour.get(level);
+                if (distance + shiftValue < minDistance) {
+                    shiftValue = minDistance - distance
+                }
+            }
+
+            if (shiftValue > 0) {
+                box.setX(box.coord.x + shiftValue);
+                box.mod += shiftValue;
+
+                // this.centerNodeBetween(id, sibling);
+
+                shiftValue = 0;
+            }
+
+            siblingId = this.boxes.getNextSibling(siblingId);
+            sibling = this.boxes.getBox(siblingId);
+        }
+    }
+
+    getLeftContour(id, modSum, values) {
+        const box = this.boxes.getBox(id);
+        const childIds = this.boxes.getConnections(id);
+
+        if (!values.has(box.coord.y)) {
+            values.set(box.coord.y, box.coord.x + modSum);
+        } else {
+            let oldVal = values.get(box.coord.y);
+            let newVal = Math.min(oldVal, box.coord.x + modSum);
+            values.set(box.coord.y, newVal);
+        }
+
+        modSum += box.mod;
+
+        for (const cid of childIds) {
+            this.getLeftContour(cid, modSum, values);
+        }
+    }
+
+    getRightContour(id, modSum, values) {
+        const box = this.boxes.getBox(id);
+        const childIds = this.boxes.getConnections(id);
+
+        if (!values.has(box.coord.y)) {
+            values.set(box.coord.y, box.coord.x + modSum);
+        } else {
+            let oldVal = values.get(box.coord.y);
+            let newVal = Math.max(oldVal, box.coord.x + modSum);
+            values.set(box.coord.y, newVal);
+        }
+
+        modSum += box.mod;
+
+        for (const cid of childIds) {
+            this.getRightContour(cid, modSum, values);
+        }
+    }
+}
+
+const getMaxKey = (map) => {
+    let keys = Array.from(map.keys());
+    return Math.max(...keys);
 }
 
 export { TreeFormatter };
