e629751eeb410ee6d626396700c5f6c3439acefa
Acaruso <anthonycaruso102@gmail.com>
2022-03-09 15:54:28 -0500

    stuff

--- example_scripts/coin_change_backtracking3.js
+++ example_scripts/coin_change_backtracking3.js
@@ -8,25 +8,13 @@
 
 function userFunction(logger) {
     function logEntrypoint(i, coins, amount, parentId) {
-        let curCoinStr = "";
-
-        if (i < coins.length) {
-            curCoinStr = coins[i];
-        } else {
-            curCoinStr = "no coin";
-        }
-
-        let str = "";
-        str += "i: " + i;
-        str += "\ncurCoin: " + curCoinStr;
-        str += "\namount: " + amount;
+        let str = `helper(${i}, [${coins}], ${amount})\n`
         const id = logger.newNode(str, parentId);
-
         return id;
     }
 
     function logReturn(val, id) {
-        logger.appendToNode("\n-> " + val, id);
+        logger.appendToNode("\n \n-> " + val, id);
     }
 
     function append(val, id) {
@@ -38,16 +26,17 @@ function userFunction(logger) {
     function helper({ i, coins, amount, parentId }) {
         const id = logEntrypoint(i, coins, amount, parentId);
 
+        let curRes = { found: false, numCoins: MAX_INT };
+
         if (amount === 0) {
-            logReturn(`{ found: true, numCoins: 0 }`, id);
-            return { found: true, numCoins: 0 };
+            curRes = { found: true, numCoins: 0 };
+            logReturn(JSON.stringify(curRes), id);
+            return curRes;
         }
 
         if (i < coins.length && amount > 0) {
             const curCoin = coins[i];
             const maxNumCurCoins = Math.floor(amount / curCoin);
-
-            let curRes = { found: false, numCoins: MAX_INT };
 
             for (let numCurCoins = 0; numCurCoins <= maxNumCurCoins; numCurCoins++) {
                 if (numCurCoins * curCoin <= amount) {
@@ -58,7 +47,10 @@ function userFunction(logger) {
                         parentId: id,
                     });
 
-                    append(JSON.stringify(res), id);
+                    // `helper(${i}, [${coins}], ${amount})`
+
+                    append(`helper(${i + 1}, [${coins}], ${amount - (numCurCoins * curCoin)})`, id);
+                    append("-> " + JSON.stringify(res), id);
 
                     if (res.found) {
                         curRes.numCoins = Math.min(
@@ -70,8 +62,6 @@ function userFunction(logger) {
                 }
             }
 
-            // logger.appendToNode(`\nminNumCoins:${minNumCoins}`, id);
-
             if (curRes.found === false) {
                 logReturn(JSON.stringify(curRes), id);
                 return { found: false, numCoins: 0 };
@@ -82,7 +72,8 @@ function userFunction(logger) {
         }
 
         logReturn("{ found: false, numCoins: 0 }", id);
-        return { found: false, numCoins: 0 };
+        return curRes;
+        // return { found: false, numCoins: 0 };
     }
 
     function coinChange(coins, amount) {
