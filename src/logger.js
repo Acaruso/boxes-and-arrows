e0278a550c70f1969e478059f6cfd67f799cab15
Acaruso <anthonycaruso102@gmail.com>
2022-03-20 16:00:27 -0400

    stuff

---
+++ src/logger.js
@@ -0,0 +1,48 @@
+class Logger {
+    constructor(boxes) {
+        this.boxes = boxes;
+        this.rootNodeId = null;
+        this.enabled = true;
+    }
+
+    enable() {
+        this.enabled = true;
+    }
+
+    disable() {
+        this.enabled = false;
+    }
+
+    newNode(str, parentId) {
+        if (this.enabled === false) {
+            return;
+        }
+
+        const id = this.boxes.addBox("", { x: 0, y: 0 });
+        const box = this.boxes.getBox(id);
+        box.appendString(str);
+
+        if (this.rootNodeId === null) {
+            this.rootNodeId = id;
+        }
+
+        if (parentId !== null) {
+            this.boxes.addConnection(parentId, id);
+        }
+
+        return id;
+    }
+
+    appendToNode(s, id) {
+        if (this.enabled === false) {
+            return;
+        }
+
+        const box = this.boxes.getBox(id);
+        if (box !== null && box !== undefined) {
+            box.appendString(s);
+        }
+    }
+}
+
+export { Logger };
--- src/scripter.js
+++ src/scripter.js
@@ -1,55 +1,8 @@
 import { getAllIdsInTree, moveBoxesByDelta } from "./tree_util";
-
-class Logger {
-    constructor(boxes) {
-        this.boxes = boxes;
-        this.rootNodeId = null;
-        this.enabled = true;
-    }
-
-    enable() {
-        this.enabled = true;
-    }
-
-    disable() {
-        this.enabled = false;
-    }
-
-    newNode(str, parentId) {
-        if (this.enabled === false) {
-            return;
-        }
-
-        const id = this.boxes.addBox("", { x: 0, y: 0 });
-        const box = this.boxes.getBox(id);
-        box.appendString(str);
-
-        if (this.rootNodeId === null) {
-            this.rootNodeId = id;
-        }
-
-        if (parentId !== null) {
-            this.boxes.addConnection(parentId, id);
-        }
-
-        return id;
-    }
-
-    appendToNode(s, id) {
-        if (this.enabled === false) {
-            return;
-        }
-
-        const box = this.boxes.getBox(id);
-        if (box !== null && box !== undefined) {
-            box.appendString(s);
-        }
-    }
-}
+import { Logger } from "./logger";
 
 class Scripter {
     constructor(model, treeFormatter) {
-        this.model = model;
         this.boxes = model.boxes;
         this.treeFormatter = treeFormatter;
         this.logger = new Logger(this.boxes);
