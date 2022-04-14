4cba41955398fd89cfc82fc107d3048267ab31c9
Acaruso <anthonycaruso102@gmail.com>
2022-04-14 06:09:19 -0400

    stuff

--- example_scripts/tree_height.js
+++ example_scripts/tree_height.js
@@ -36,16 +36,29 @@ function userFunction(logger) {
 
         const leftHeight = getHeight(node.left, id);
         logger.appendToNode(`getHeight(node.left) -> ${leftHeight}\n`, id);
-        
+
         const rightHeight = getHeight(node.right, id);
         logger.appendToNode(`getHeight(node.right) -> ${rightHeight}\n`, id);
 
         const curHeight = Math.max(leftHeight, rightHeight) + 1;
 
+        logger.appendToNode(`\ncurHeight = max(${leftHeight}, ${rightHeight}) + 1 -> ${curHeight}\n`, id);
+
         logger.appendToNode(`\n-> ${curHeight}`, id);
 
         return curHeight;
     }
+
+    // function getHeight(node, parentId) {
+    //     const id = logger.newNode(`getHeight(${node.value})\n\n`, parentId);
+
+    //     const leftRes = node.left ? getHeight(node.left, id) : 0;
+    //     const rightRes = node.right ? getHeight(node.right, id) : 0;
+
+    //     const res = Math.max(leftRes, rightRes) + 1;
+    //     logger.appendToNode(`-> ${res}`, id);
+    //     return res;
+    // }
 
     const res = getHeight(root, null);
 
