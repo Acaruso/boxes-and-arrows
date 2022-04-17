0a8e1b1ff0f8da835edc1840e9bd22dcb0bcc4ae
Acaruso <anthonycaruso102@gmail.com>
2022-04-17 14:28:00 -0400

    stuff

--- example_scripts/partition.js
+++ example_scripts/partition.js
@@ -15,41 +15,18 @@ function userFunction(logger) {
         return colors;
     }
 
-    function pushArr1(arr, id) {
-        // const colors = [["yellow", 0]];
+    function pushArrInit(arr, id) {
         const colors = getColors(arr);
         logger.appendArrayToNode(arr, [], id, colors);
     }
 
-    function pushArr2(arr, i, id) {
-        const labels = [["i", i]];
-        // const colors = [["yellow", 0], ["blue", 1, i - 1], ["pink", i]];
+    function pushArr(arr, i, j, id) {
+        const labels = [["i", i], ["j", j]];
         const colors = getColors(arr);
         logger.appendArrayToNode(arr, labels, id, colors);
     }
 
-    // function pushArr3(arr, i, j, id) {
-    //     const labels = [["i", i], ["j", j]];
-    //     const colors = [["yellow", 0], ["blue", 1, i - 1], ["pink", i, j - 1]];
-    //     logger.appendArrayToNode(arr, labels, id, colors);
-    // }
-
-    function pushArr3(arr, i, j, id) {
-        const labels = [["i", i], ["j", j]];
-
-        let colors = [["yellow", 0]];
-        for (let i = 1; i < arr.length; i++) {
-            if (arr[i] < arr[0]) {
-                colors.push(["blue", i])
-            } else {
-                colors.push(["pink", i])
-            }
-        }
-
-        logger.appendArrayToNode(arr, labels, id, colors);
-    }
-
-    function pushArr4(arr, i, j, id) {
+    function pushArrFinal(arr, i, j, id) {
         const labels = [["i", i], ["j", j]];
         const colors = [["yellow", i - 1], ["blue", 0, i - 2], ["pink", i, arr.length]];
         logger.appendArrayToNode(arr, labels, id, colors);
@@ -63,45 +40,47 @@ function userFunction(logger) {
     
     function partition(arr) {
         let id = logger.newNode("initial state:", null);
-
-        pushArr1(arr, id);
+        pushArrInit(arr, id);
 
         let p = arr[0];
         let i = 1;
-        let j = 1;
 
         while (arr[i] < p) {
             i++;
         }
 
-        pushStr("\nfind first elt >= p:", id);
-        pushArr2(arr, i, id);
+        let j = i + 1;
 
-        for (j = i + 1; j < arr.length; j++) {
+        pushStr("\ni = first elt >= p:", id);
+        pushStr("\nj = i + 1:", id);
+        pushArr(arr, i, j, id);
+
+        for (; j < arr.length; j++) {
             pushStr("----------------------------------------", id);
-            pushStr("\nj++:", id);
-            pushArr3(arr, i, j, id);
+            pushArr(arr, i, j, id);
             if (arr[j] < p) {
                 pushStr("\nswap i <-> j:", id);
                 swap(arr, i, j);
-                pushArr3(arr, i, j, id);
+                pushArr(arr, i, j, id);
                 pushStr("\ni++:", id);
                 i++;
-                pushArr3(arr, i, j, id);
+                pushArr(arr, i, j, id);
             }
+            pushStr("\nj++:", id);
         }
 
         pushStr("final ----------------------------------", id);
-        pushArr3(arr, i, j, id);
+        pushArr(arr, i, j, id);
         pushStr("\nswap 0 <-> (i - 1):", id);
         swap(arr, 0, i - 1);
-        pushArr4(arr, i, j, id);
+        pushArrFinal(arr, i, j, id);
         pushStr(`-> pivot: ${i - 1}`, id);
     
         return i - 1;
     }
 
-    // const arr = [5, 3, 5, 6, 0, 1];
     const arr = [5,3,5,0,6,1,2,3,8,6];
+    // const arr = [5, 3, 5, 6, 0, 1];
+    // const arr = [5,6,3,1,7];
     partition(arr);
 }
