158fa5d9ad6d21879ed253d5fbf9a2e678989dd0
Acaruso <anthonycaruso102@gmail.com>
2022-06-04 13:12:30 -0400

    stuff

--- example_scripts/magic_index.js
+++ example_scripts/magic_index.js
@@ -35,54 +35,6 @@ function userFunction(logger) {
     }
 
     // books:
-
-    function magicIndexNotUnique(arr) {
-        let res = null;
-
-        function inner(l, r, parentId) {
-            let id = logger.newNode(`inner(${l}, ${r})`, parentId);
-
-            if (l > r || res !== null) {
-                return;
-            }
-
-            let midIdx = Math.floor((l + r) / 2);
-            let midElt = arr[midIdx];
-
-            pushArray(arr, l, r, midIdx, id);
-
-            if (midElt === midIdx) {
-                logger.pushLine(`found: ${midIdx}`, id);
-                res = midIdx;
-                return;
-            }
-
-            let newR = Math.min(midIdx - 1, midElt);
-            inner(l, newR, id);
-            logger.pushLine(`inner(${l}, ${newR})`, id);
-
-            let newL = Math.max(midIdx + 1, midElt);
-            inner(newL, r, id);
-
-            // let newR = Math.min(midIdx - 1);
-            // inner(l, newR, id);
-            // logger.pushLine(`inner(${l}, ${newR})`, id);
-
-            // let newL = Math.max(midIdx + 1);
-            // inner(newL, r, id);
-
-            logger.pushLine(`inner(${newL}, ${r})`, id);
-        }
-
-        inner(0, arr.length - 1, null);
-
-        return res;
-    }
-
-
-
-    // mine:
-
     // function magicIndexNotUnique(arr) {
     //     let res = null;
 
@@ -99,26 +51,19 @@ function userFunction(logger) {
     //         pushArray(arr, l, r, midIdx, id);
 
     //         if (midElt === midIdx) {
+    //             logger.pushLine(`found: ${midIdx}`, id);
     //             res = midIdx;
     //             return;
     //         }
 
-    //         if (midElt < midIdx) {
+    //         let newR = Math.min(midIdx - 1, midElt);
+    //         inner(l, newR, id);
+    //         logger.pushLine(`inner(${l}, ${newR})`, id);
 
-    //             // recurse left
-    //             inner(l, midElt, id);
+    //         let newL = Math.max(midIdx + 1, midElt);
+    //         inner(newL, r, id);
 
-    //             // recurse right
-    //             inner(midIdx + 1, r, id);
-
-    //         } else if (midElt > midIdx) {
-
-    //             // recurse left
-    //             inner(l, midIdx - 1, id);
-
-    //             // recurse right
-    //             inner(midIdx + 1, midElt, id);
-    //         }
+    //         logger.pushLine(`inner(${newL}, ${r})`, id);
     //     }
 
     //     inner(0, arr.length - 1, null);
@@ -126,12 +71,56 @@ function userFunction(logger) {
     //     return res;
     // }
 
+    // mine:
+    function magicIndexNotUnique(arr) {
+        let res = null;
+
+        function inner(l, r, parentId) {
+            let id = logger.newNode(`inner(${l}, ${r})`, parentId);
+
+            if (l > r || res !== null) {
+                return;
+            }
+
+            let m = Math.floor((l + r) / 2);
+
+            pushArray(arr, l, r, m, id);
+
+            if (arr[m] === m) {
+                logger.pushLine(`found: ${m}`, id);
+                res = m;
+                return;
+            } else if (arr[m] < m) {
+
+                // left
+                inner(l, arr[m], id);
+
+                // right
+                inner(m + 1, r, id);
+
+            } else if (arr[m] > m) {
+                
+                // left
+                inner(l, m - 1, id);
+
+                // right
+                inner(arr[m], r, id);
+            }
+        }
+
+        inner(0, arr.length - 1, null);
+
+        return res;
+    }
+
+
     // let arr = [-1, 1, 1, 1, 1, 1, 9, 9, 9];
 
     // non-unique example from book:
-    // let arr = [-9, -5, 2, 2, 2, 3, 4, 7, 9, 12, 13];
+    let arr = [-9, -5, 2, 2, 2, 3, 4, 7, 9, 12, 13];
 
-    let arr = [-11, -1, -1, 2, 7, 8, 9, 9, 9, 9, 10, 10, 13, 15, 17, 17, 22];
+    // let arr = [-11, -1, -1, 2, 7, 8, 9, 9, 9, 9, 10, 10, 13, 15, 17, 17, 22];
+
     // let arr = [-1, 0, 1, 2, 3, 5, 7];
 
     // let arr = [1, 2, 2, 4, 4, 5];
